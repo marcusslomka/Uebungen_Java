f3420cca5de0eaede57a0fa4d94fcb5d0cd8db40
marcusslomka <marcus.slomka@googlemail.com>
2024-06-26 16:22:52 +0200

    done

---
+++ Day3_24.06.26/SchleifenUmwandeln2.java
@@ -0,0 +1,20 @@
+public class SchleifenUmwandeln2 {
+    //  2.Teil der Umwandlungsuafgabe
+    public static void main(String[] args) {
+        // gegebene While-Schleife
+        int a = 0;
+        do {
+            a += 2;
+            System.out.println(a);
+        } while (a < 7);
+
+        // umgewandelte For-Schleife
+        
+        
+        for (int u = 0; u < 7;){
+            u +=2;
+            System.out.println(u);
+        }
+    }
+    
+}
--- Day3_24.06.26/firstLoop.java
+++
@@ -1,6 +0,0 @@
-public class firstLoop {
-    public static void main(String[] args) {
-        int reverse [19,0,239,30,459,30,29];
-        System.out.println();
-    }
-}
