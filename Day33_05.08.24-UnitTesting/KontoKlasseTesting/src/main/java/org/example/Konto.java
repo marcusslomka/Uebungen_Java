93d9a9483158e830c1b5a045d6fb1b2b1869aad1
marcusslomka <marcus.slomka@googlemail.com>
2024-08-05 14:22:29 +0200

    Testbereit

---
+++ Day33_05.08.24-UnitTesting/KontoKlasseTesting/src/main/java/org/example/Konto.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Konto {
+    private String kontonummer;
+    private double saldo;
+
+    public Konto(String kontonummer, double anfangssaldo){
+        this.kontonummer = kontonummer;
+        this.saldo = anfangssaldo;
+    }
+
+    public void einzahlen(double betrag){
+        if(betrag < 0) throw new IllegalArgumentException("Betrag muss positiv sein!");
+        this.saldo += betrag;
+    }
+    public void abheben(double betrag){
+        if (betrag < 0) throw new IllegalArgumentException("Betrag muss positiv sein!");
+        if (this.saldo - betrag < 0) throw new IllegalArgumentException("Das Konto darf nicht überzogen werden!")
+        this.saldo -= betrag;
+    }
+    public double getSaldo(){
+        return this.saldo;
+    }
+    public String getKontonummer(){
+        return this.kontonummer;
+    }
+    public void ueberweisen(Konto zielkonto, double betrag){
+        if (this.saldo - betrag < 0) throw new IllegalArgumentException("Das Konto hat nicht genügend Geld um die Überweisung zu tätigen!");
+        this.saldo -= betrag;
+        zielkonto.einzahlen(betrag);
+    }
+}
