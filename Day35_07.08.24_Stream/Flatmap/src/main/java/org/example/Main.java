94766b7215e8aae72a341c38fa28284752a2b442
marcusslomka <marcus.slomka@googlemail.com>
2024-08-13 15:13:58 +0200

    done

--- Day35_07.08.24_Stream/Flatmap/src/main/java/org/example/Main.java
+++ Day35_07.08.24_Stream/Flatmap/src/main/java/org/example/Main.java
@@ -1,30 +1,24 @@
 package org.example;
 
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         // 1. Liste von Listen zu einer einzigen Liste "flach machen"
-        ArrayList<ArrayList> großeListe = new ArrayList<>();
-        ArrayList<Integer> l1 = new ArrayList<>();
-        ArrayList<Integer> l2 = new ArrayList<>();
-        ArrayList<Integer> l3 = new ArrayList<>();
-        l1.add(1);
-        l1.add(2);
-        l2.add(3);
-        l2.add(4);
-        l3.add(5);
-        l3.add(6);
-        großeListe.add(l1);
-        großeListe.add(l2);
-        großeListe.add(l3);
-        //Wie kriegt man das kürzer hin?!?
-
-        großeListe
+        List<List<Integer>> listOfLists = Arrays.asList(
+                Arrays.asList(1,2),
+                Arrays.asList(3,4),
+                Arrays.asList(5,6)
+        );
+        List<Integer>ergebnisListe = listOfLists
                 .stream()
-                .;
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+        System.out.println(ergebnisListe);
 
     }
 }
